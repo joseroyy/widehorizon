4fdbce39584bc0a08f01246ec9e26d3fec3ca04b
Niradeepam <joseyor14@gmail.com>
2026-04-07 00:38:16 +0530

    Fixed mobile menu and added Services link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <nav style={{ padding: '28px 52px' }}>
-      <Link to="/" className="logo">
+      <Link to="/" className="logo" onClick={() => setMenuOpen(false)}>
         <div className="logo-mark">
           <img src="/logo.png" alt="Wide Horizon Logo" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
         </div>
@@ -22,8 +22,9 @@ const Navbar = () => {
       
       <div className="nav-r">
         <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-          <li><Link to="/" className={location.pathname === '/' ? 'active' : ''} onClick={() => setMenuOpen(false)}>Home</Link></li>
+          <li><Link to="/" className={location.pathname === '/' && !location.hash ? 'active' : ''} onClick={() => setMenuOpen(false)}>Home</Link></li>
           <li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''} onClick={() => setMenuOpen(false)}>About</Link></li>
+          <li><a href="/#services" className={location.hash === '#services' ? 'active' : ''} onClick={() => setMenuOpen(false)}>Services</a></li>
           <li><Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''} onClick={() => setMenuOpen(false)}>Contact</Link></li>
         </ul>
         
